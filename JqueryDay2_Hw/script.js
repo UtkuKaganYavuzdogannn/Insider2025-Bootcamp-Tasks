11f9df87136ed1ab4b6e7c8f94c55576442bc7d1
UtkuKaganYavuzdogannn <Utku_K_Y@outlook.com>
2025-07-17 18:04:50 +0300

    Get ile ajax ve jquery

---
+++ JqueryDay2_Hw/script.js
@@ -0,0 +1,49 @@
+let postIlk = 0;
+let postSon = 7;
+let yuklemeDurumu= false; 
+
+$(document).ready(function () {
+  loadPosts();
+});
+
+
+// Kaydırma eventinin tetiklenmesi
+  $(window).scroll(function () {
+    // Sayfanın sonuna yakın mıyız kontrolü
+    if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+      if (!yuklemeDurumu) {
+        loadPosts();
+      }
+    }
+  });
+
+// Postları çeken fonksiyon ve yüklemenin başlaması
+function loadPosts() {
+    yuklemeDurumu = true;  
+  $("#loading").show(); 
+
+  $.get(`https://jsonplaceholder.typicode.com/posts?_start=${postIlk}&_limit=${postSon}`, function (data) {
+    data.forEach(function (post) 
+    {
+      $("#postContainer").append
+      (`
+        <div class="post">
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        </div>
+
+      `);
+    });
+
+   //  +5 daha yüklenmesi icin 
+    postIlk += postSon;
+    yuklemeDurumu = false;
+    $("#loading").hide(); 
+  })
+
+  .fail(function() {
+    $("#loading").hide();
+    alert("Post yuklemesi basarısız....");
+    yuklemeDurumu = false;
+  });
+}
